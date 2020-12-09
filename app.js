e056238822f552a51aa493686d87a951057debe3
Ryan Reiterman <ryreiterman@gmail.com>
2020-12-08 20:27:17 -0500

    Flip cards working

--- app.js
+++ app.js
@@ -19,10 +19,182 @@
 
 // //Repeat until Round 3, at end of round 3, message displays with the winner
 
-// // --------------------------- //
+// // ----------------------------------------------------- //
+
+$(() => {
+
+    const cardArray = [
+      {
+        name: "Cookie Monster",
+        img: "images/cookie_monster_200.jpg"
+      },
+      {
+        name: "Cookie Monster",
+        img: "images/cookie_monster_200.jpg"
+      },
+      {
+        name: "Elmo",
+        img: "images/elmo_200.jpg"
+      },
+      {
+        name: "Elmo",
+        img: "images/elmo_200.jpg"
+      },
+      {
+        name: "Bert",
+        img: "images/bert_200.jpg"
+      },
+      {
+        name: "Bert",
+        img: "images/bert_200.jpg"
+      },
+      {
+        name: "Ernie",
+        img: "images/ernie_200.jpg"
+      },
+      {
+        name: "Ernie",
+        img: "images/ernie_200.jpg"
+      },
+      {
+        name: "Count",
+        img: "images/count_200.jpg"
+      },
+      {
+        name: "Count",
+        img: "images/count_200.jpg"
+      },
+      {
+        name: "Grover",
+        img: "images/grover_200.jpg"
+      },
+      {
+        name: "Grover",
+        img: "images/grover_200.jpg"
+      }
+    ];
+
+    const $board = $(".cards-wrapper");
+    
+    const $cardsChosen = [];
+    const $cardsChosenId = [];
+
+    //Loop over each card in the array, create img element for each card, add logo to each card, add event listener to each card, append to cards-wrapper
+    const buildBoard = () => {
+        for(i = 0; i < cardArray.length; i++) {
+            let $card = $("<img>");
+            $card.attr("src", "images/logo_200.jpg");
+            $card.attr("data-id", i);
+            $card.attr("data-img", cardArray[i].img);
+            $card.on("click", flipCard);
+            $board.append($card);
+        }
+        
+    }
+
+    //check if cards match
+    const checkForMatch = () => {
+        const $cards = $("img");
+        const $cardOneId = $($cardsChosenId[0]);
+        const $cardTwoId = $($cardsChosenId[1]);
+
+        if($cardsChosenId[0] === $cardsChosenId[1]) {
+            
+            alert("Match!");
+        }
+        else {
+            alert("Not a match");
+        }
+    }
+
+    //flip cards
+
+    const flipCard = event => {
+        
+
+        const $dataId = $(event.currentTarget).data("id");
+        console.log($dataId);
+
+        $cardsChosen.push(cardArray[$dataId].name);
+        console.log($cardsChosen);
+
+        $cardsChosenId.push($dataId);
+        console.log($cardsChosenId);
+
+        $("img").eq($dataId).attr("src", cardArray[$dataId].img);
+
+        if($cardsChosen.length === 2) {
+            checkForMatch();
+        }
+        
+    }
+
+
+    
+    buildBoard();
+
+
+//   const board = $("div#board");
+//   console.log(board);
+//   const data = [
+//     {
+//       name: "a",
+//       id: 0,
+//     },
+//     {
+//       name: "b",
+//       id: 1,
+//     },
+//     {
+//       name: "c",
+//       id: 2,
+//     },
+//     {
+//       name: "d",
+//       id: 3,
+//     },
+//     {
+//       name: "a",
+//       id: 4,
+//     },
+//     {
+//       name: "b",
+//       id: 5,
+//     },
+//     {
+//       name: "c",
+//       id: 6,
+//     },
+//     {
+//       name: "d",
+//       id: 7,
+//     },
+//   ];
+//   const buildBoard = (el, list) => {
+//     return el.html(
+//       list
+//         .map((item) => {
+//           return `<div data-name="${item.name}" id="${item.id}">${item.name}</div>`;
+//         })
+//         .join("")
+//     );
+//   };
+//   const $board = $(buildBoard(board, data));
+//   let valuesToCheck = [];
+//   for (let i = 0; i < $board.children().length; i++) {
+//     $board
+//       .children()
+//       .eq(i)
+//       .on("click", () => {
+//         valuesToCheck.push($board.children().eq(i));
+//       });
+//   }
+});
 
 
 
+
+// // First version ----------------------------------------------------- //
 
 // //Game Class
 // class MatchGame {
